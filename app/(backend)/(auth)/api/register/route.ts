4c004a4030c41f0ce7e7d73e8f336539d09ebc30
Asyrafi0257 <asyrafi0257@gmail.com>
2026-08-04 01:32:28 +0800

    fixed: fix problem typescript

--- app/(backend)/(auth)/api/register/route.ts
+++ app/(backend)/(auth)/api/register/route.ts
@@ -85,7 +85,7 @@ export async function POST(request : NextRequest){
             status: 200,
         })
     }catch(error){
-        console.error("Internal server problem, Try again!")
+        console.log("Internal server problem, Try again!", error)
         return NextResponse.json({
             success: false,
             message : " Internal server problem, please try again!"
