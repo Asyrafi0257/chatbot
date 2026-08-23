e1454a08c78c0623884a46a1de943437c15b43f2
Asyrafi0257 <asyrafi0257@gmail.com>
2026-08-24 01:41:44 +0800

    feat: add ui for chat page

--- app/(frontend)/(main)/chat/page.tsx
+++ app/(frontend)/(main)/chat/page.tsx
@@ -2,8 +2,17 @@
 
 export default function Chat(){
     return (
-        <div>
-            <h2>Chatting page</h2>
+        <div className="h-screen bg-[#0F172A] flex justify-center items-center">
+            <div className="relative bg-[#1E293B] w-[800px] h-[700px] rounded-xl shadow-[0_0_10px_#facc15] p-5">
+
+                {/* input for ask AI */}
+                <div className="absolute w-full flex items-center bottom-[20px] justify-center">
+                    <div className="w-[40em] h-[2em] outline outline-1 outline-offset-1 outline-yellow-400 focus-within:outline-2 focus-within:outline-offset-2 focus-whithin:outline-yellow-600 rounded-2xl flex items-center pl-[2px]">
+                        <textarea className="w-full h-full outline-none border-none text-white resize-none overflow-y-auto break-words p-[3px]" placeholder="Ask your AI assistance"/>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
